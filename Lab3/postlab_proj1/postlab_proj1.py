8184b0e26518a43f3eb552a750c4e793842934cf
DustinFernandez123 <163655774+DustinFernandez123@users.noreply.github.com>
2025-08-02 20:19:01 +0800

    Add files via upload

---
+++ Lab3/postlab_proj1/postlab_proj1.py
@@ -0,0 +1,63 @@
+class Student(object):
+    """Represents a student."""
+
+    def __init__(self, name, number):
+        """All scores are initially 0."""
+        self.name = name
+        self.scores = []
+        for count in range(number):
+            self.scores.append(0)
+
+    def getName(self):
+        """Returns the student's name."""
+        return self.name
+  
+    def setScore(self, i, score):
+        """Resets the ith score, counting from 1."""
+        self.scores[i - 1] = score
+
+    def getScore(self, i):
+        """Returns the ith score, counting from 1."""
+        return self.scores[i - 1]
+   
+    def getAverage(self):
+        """Returns the average score."""
+        return sum(self.scores) / len(self.scores)
+    
+    def getHighScore(self):
+        """Returns the highest score."""
+        return max(self.scores)
+ 
+    def __str__(self):
+        """Returns the string representation of the student."""
+        return "Name: " + self.name  + "\nScores: " + \
+               " ".join(map(str, self.scores))
+
+    def __eq__(self, other):
+        """Tests for equality of two Student objects based on their names."""
+        if isinstance(other, Student):
+            return self.name == other.name
+        return False
+
+    def __lt__(self, other):
+        """Tests if this Student is less than another Student based on their names."""
+        if isinstance(other, Student):
+            return self.name < other.name
+        return NotImplemented
+
+    def __ge__(self, other):
+        """Tests if this Student is greater than or equal to another Student based on their names."""
+        if isinstance(other, Student):
+            return self.name >= other.name
+        return NotImplemented
+
+def main():
+    """A simple test."""
+    student = Student("Ken", 5)
+    print(student)
+    for i in range(1, 6):
+        student.setScore(i, 100)
+    print(student)
+
+if __name__ == "__main__":
+    main()
